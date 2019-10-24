57556d7c5175e1882be171453ddf99a1963c4e44
jz93 <box@unitycode.tech>
2019-10-23 21:19:28 -0300

    Perguntar o termo de busca e perguntar o prefixo

--- index.js
+++ index.js
@@ -1,12 +1,24 @@
+const readline = require('readline-sync');
+
 function start()
 {
     const content = {};
 
     content.searchTerm = askAndReturnSearchTerm();
+    content.prefix = askAndReturnPrefix();
 
     function askAndReturnSearchTerm()
     {
-        return "TERM EXAMPLE";
+        return readline.question('Digite um termo para buscar na Wikipedia ');
+    }
+
+    function askAndReturnPrefix()
+    {
+        const prefixes = ['Quem é', 'O que é', 'A história de'];
+        const selectedPrefixIndex = readline.keyInSelect(prefixes);
+        const selectedPrefixText = prefixes[selectedPrefixIndex];
+
+        return selectedPrefixText;
     }
 
     console.log(content);
