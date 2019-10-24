0dae5673ebc1d591c65f3e747622735e4c7952bb
jz93 <box@unitycode.tech>
2019-10-23 21:08:38 -0300

    add biblioteca readline-sync do node

---
+++ index.js
@@ -0,0 +1,14 @@
+function start()
+{
+    const content = {};
+
+    content.searchTerm = askAndReturnSearchTerm();
+
+    function askAndReturnSearchTerm()
+    {
+        return "TERM EXAMPLE";
+    }
+
+    console.log(content);
+}
+start();
