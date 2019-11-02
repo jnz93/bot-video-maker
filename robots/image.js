0ce6a0e79fa8f63d79e601a873f806638545e15a
jz93 <box@unitycode.tech>
2019-11-02 17:13:01 -0300

    Feat: add image download logig with fallback

--- robots/image.js
+++ robots/image.js
@@ -1,3 +1,4 @@
+const imageDownloader = require('image-downloader');
 const google = require('googleapis').google;
 const customSearch = google.customsearch('v1');
 const state = require('./state.js');
@@ -9,7 +10,7 @@ async function robot()
     const content = state.load();
     
     await fetchImagesOfAllSentences(content);
-
+    await downloadAllImages(content);
     state.save(content);
 
     async function fetchImagesOfAllSentences(content)
@@ -37,6 +38,47 @@ async function robot()
             return item.link;
         });
         return imagesUrl;
+    }
+
+    async function downloadAllImages(content)
+    {
+        content.downloadedImages = [];
+
+        content.sentences[1].images[0] = 'https://sites.google.com/site/aprendiendosobreelbasquetbol/_/rsrc/1253772100621/michael-jordan-la-leyenda-/michael-jordan-mj-mj-23.jpg?height=471&width=295';
+
+        for (let sentenceIndex = 0; sentenceIndex < content.sentences.length; sentenceIndex++)
+        {
+            const images = content.sentences[sentenceIndex].images;
+
+            for (let imageIndex = 0; imageIndex < images.length; imageIndex++)
+            {
+                const imageUrl = images[imageIndex];
+
+                try
+                {
+                    if (content.downloadedImages.includes(imageUrl))
+                    {
+                        throw new Error('> Image já foi baixada.');
+                    }
+
+                    await downloadAndSave(imageUrl, `${sentenceIndex}-original.png`);
+                    content.downloadedImages.push(imageUrl);
+                    console.log(`> [${sentenceIndex}][${imageIndex}] Baixou a imagem com sucesso: ${imageUrl}`);
+                    break;
+                } catch (error)
+                {
+                    console.log(`> Erro ao baixar (${imageUrl}): ${error}`);
+                }
+            }
+        }
+    }
+
+    async function downloadAndSave(url, fileName)
+    {
+        return imageDownloader.image({
+            url, url,
+            dest: `./content/${fileName}`
+        });
     }
 }
 module.exports = robot;
